d9c09eb1b5408047db93eb419fd5785db98a58af
rohmju <rohmju@media-saturn.com>
2025-03-31 13:22:28 +0200

    added v1
    Adding the first Idea of the Website

---
+++ code/Main/script.js
@@ -0,0 +1,56 @@
+function kanyeWest() {
+    const body = document.querySelector('html');
+    body.innerHTML = '';
+    const iframe = document.createElement('iframe'); 
+    const returnbutton = document.createElement('button');
+    iframe.src = 'https://www.youtube.com/embed/0i1sbW26-K8?autoplay=1&loop=1&playlist=0i1sbW26-K8&mute=1';
+    iframe.style.position = 'fixed';
+    iframe.style.top = '0';
+    iframe.style.left = '0';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.border = 'none';
+    iframe.style.zIndex = '-1'; 
+    returnbutton.innerText = 'Return';
+    returnbutton.style.position = 'fixed';
+    returnbutton.style.top = '10px';
+    returnbutton.style.left = '10px';
+    returnbutton.style.zIndex = '1';
+    returnbutton.setAttribute('id', 'returnbutton',
+    );
+    returnbutton.addEventListener('mouseover', () => {goofyahhmove(returnbutton)});
+        
+        
+    returnbutton.addEventListener('click', () => {
+        window.location.reload();
+    });
+    body.appendChild(iframe);
+    body.appendChild(returnbutton);
+}
+function Kanyesouth() {
+    const body = document.querySelector('body');
+    body.innerHTML = '';
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://www.youtube.com/embed/0i1sbW26-K8?autoplay=1&loop=1&playlist=0i1sbW26-K8&mute=1';
+    const returnbutton = document.createElement('button');
+    returnbutton.innerText = 'Return';
+    returnbutton.addEventListener('click', () => {
+        window.location.reload();
+    });
+
+    body.appendChild(iframe);
+    body.appendChild(returnbutton);
+}
+function goofyahhmove(returnbutton) {
+    returnbutton.style.left = "2000px";
+    returnbutton.style.backgroundImage = 'url("https://th.bing.com/th/id/OIP.EWgi7rIvjjBXCfZLH1LDJgAAAA?rs=1&pid=ImgDetMain")';
+    returnbutton.style.height = '180px';
+    returnbutton.style.width = '180px';
+    returnbutton.innerText = '';
+    returnbutton.style.backgroundSize = '180px 180px';
+    returnbutton.style.transition = 'left 4s';
+    returnbutton.style.transitionDelay = '1s';
+    while (true){
+        returnbutton.style.color = 'red';
+    }
+}
