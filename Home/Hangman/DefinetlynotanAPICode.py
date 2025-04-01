2e1332f16b0e73e126397d58dd6622f7bee7f405
rohmju <rohmju@media-saturn.com>
2025-04-01 08:36:49 +0200

    Refactor .gitignore for Hangman game and secure API key handling

--- Home/Hangman/DefinetlynotanAPICode.py
+++ Home/Hangman/DefinetlynotanAPICode.py
@@ -1,10 +1,22 @@
 from google import genai
+import os
 
-client = genai.Client(api_key="example")
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents="Give me 10 Random Words to Guess in a game of HAngman. Difficultys: Easy (3-5 letters), Medium (6-8 letters), Hard (9+ letters). Make Sure to write the 10 Words only of the choosen Difficulty in JSON Format Difficulty : Easy"
-)
+file = open("APIKey.txt", "r")
+# Open the file in read mode
+with open('APIKey.txt', 'r') as file:
+    # Read the contents of the file
+    api_key = file.read()
 
-print(response.text)
+# Print the contents (API key or other data)
+print(api_key)
+
+
+#client = genai.Client(api_key="example")
+
+#response = client.models.generate_content(
+#    model="gemini-2.0-flash",
+#    contents="Give me 10 Random Words to Guess in a game of HAngman. Difficultys: Easy (3-5 letters), Medium (6-8 letters), Hard (9+ letters). Make Sure to write the 10 Words only of the choosen Difficulty in JSON Format Difficulty : Easy"
+#)
+
+#print(response.text)
