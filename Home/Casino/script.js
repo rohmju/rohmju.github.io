17426a33fc7e7bd3b3e03c362516ecae92e36d73
rohmju <rohmju@media-saturn.com>
2025-07-10 07:00:08 +0200

    update

--- Home/Casino/script.js
+++ Home/Casino/script.js
@@ -1,10 +1,13 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
 // API-Key aufteilen und erst später zusammensetzen
 const part1 = "23iujrq21eqd21e09qJKWOFLSA23WJFJa";
 const part2 = "eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoYW5mcmFqaXNyZ2hjc2t0ZHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4ODQwOTYsImV4cCI6MjA2NzQ2MDA5Nn0.";
 const part3 = "mgR_VSslwqLrvf9iE1IxRY9aUjYvSrxjUa-bfRlyRR8";
 const supabaseKey = clearlog() + part2 + part3;
+let clicked1 = false;
+let clicked2 = false;
+let mogging = false;
+const divbutton = document.getElementById("signbuttons")
 
 const supabaseUrl = "https://whanfrajisrghcsktdyv.supabase.co"
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -19,23 +22,22 @@ async function main() {
 }
 
 function appearsignupButton(){
-    const signupButton = document.createElement("button")
+    const signupButton = document.createElement("button");
     signupButton.className = "login-button";
     signupButton.textContent = "sign up!"
     signupButton.onclick = () => {showsignup()};
     signupButton.style.visibility = "visible"
-    document.body.appendChild(signupButton);
+    divbutton.appendChild(signupButton);
     console.log("signup is here ")
 }
 
 function appearLoginButton(){
     const loginButton = document.createElement('button');
-    loginButton.className = 'login-button';
+    loginButton.className = 'c';
     loginButton.textContent = 'Login';
     loginButton.onclick = () => {
-        makevisible()
-    };
-    document.body.appendChild(loginButton);
+        makevisible()}
+    divbutton.appendChild(loginButton);
     console.log('Login button appeared');
 }
 
@@ -52,6 +54,7 @@ async function fetchdata(){
 }
 
 function createloginbox(){
+    if (mogging==false){
     const loginBox = document.createElement('div');
     loginBox.className = "loginbox"
     document.body.appendChild(loginBox)    
@@ -72,13 +75,16 @@ function createloginbox(){
         if (data && data.password === password) {
             msg.textContent = 'Login successful!';
             loginBox.style.visibility = 'hidden';
+            mogging=true;
         } else {
             msg.textContent = 'Invalid username or password.';
         }
     };
+    }
 }
 
 function signup() {
+    if(mogging==false){
     const signupbox = document.createElement("div");
     signupbox.className = "signupbox";
     document.body.appendChild(signupbox);
@@ -98,6 +104,7 @@ function signup() {
     btn2.onclick = async () => {
         const username = inputfield3.value;
         const password = inputfield4.value;
+        
 
         if (!username || !password) {
             msg2.textContent = "Please fill in both fields.";
@@ -112,15 +119,47 @@ function signup() {
             msg2.textContent = "Signup failed. Username might already exist.";
         }
     }
+    }
 }
 
+
 function showsignup(){
+    if (clicked2==false){
     document.querySelector('.signupbox').style.visibility = 'visible';
+    clicked2 = true;
+    }
+    else if (clicked2==true){
+            document.querySelector('.signupbox').style.visibility = 'hidden';
+            clicked2 = false;
+
+    }
+    if (clicked1 = true){
+        document.querySelector(".loginbox").style.visibility = 'hidden';
+        clicked1=false
+    }
 }
 
 function makevisible(){
+    console.log(mogging)
+    if(mogging==false){
+    if (clicked1==false){
     document.querySelector('.loginbox').style.visibility = 'visible';
+    clicked1 = true;
+    
+    }
+    else if (clicked1==true ){
+        document.querySelector('.loginbox').style.visibility = 'hidden';
+        clicked1 = false;
+        
+    }
+    if (clicked2 = true){
+        document.querySelector(".signupbox").style.visibility = 'hidden';
+        clicked2 = false;
+    }
+    }
+    else {alert("you are already logged in ")}
 }
+
 function clearlog(){
     console.log("cleared")
     return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
